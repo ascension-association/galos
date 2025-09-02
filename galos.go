6f846356cd2e6a050f05fbc936ee8f7bbf954f57
ben-auo <180770701+ben-auo@users.noreply.github.com>
2025-09-01 23:06:53 -0700

    switch from ctr to client

--- galos.go
+++ galos.go
@@ -1,120 +1,89 @@
+// https://github.com/containerd/containerd/blob/main/docs/getting-started.md
 package main
 
-// forked from https://gokrazy.org/packages/docker-containers/
-
 import (
+	"context"
 	"fmt"
 	"log"
-	"os"
-	"os/exec"
-	"strings"
-	"context"
+	"syscall"
 	"time"
 
-	"github.com/gokrazy/gokrazy"
+	"github.com/containerd/containerd/v2/pkg/cio"
+	containerd "github.com/containerd/containerd/v2/client"
+	"github.com/containerd/containerd/v2/pkg/oci"
+	"github.com/containerd/containerd/v2/pkg/namespaces"
 )
 
-var container = "docker.io/library/hello-world:latest"
-
-func ctr(args ...string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-	defer cancel()
-	ctr := exec.CommandContext(ctx, "/usr/local/bin/ctr", args...)
-	ctr.Env = expandPath(os.Environ())
-	ctr.Stdin = os.Stdin
-	ctr.Stdout = os.Stdout
-	ctr.Stderr = os.Stderr
-	if err := ctr.Run(); err != nil {
-		return fmt.Errorf("%v: %v", ctr.Args, err)
-	}
-	return nil
-}
-
-func galos() error {
-	// Ensure we have an up-to-date clock, which in turn also means that
-	// networking is up.
-	gokrazy.WaitForClock()
-	
-	// wait a few seconds for containerd to initialize
-	time.Sleep(3 * time.Second)
-
-	task, err := exec.Command("/usr/local/bin/ctr", "task", "list", "--quiet").Output()
-	if err != nil {
-		log.Print(err)
-	}
-
-	if strings.TrimRight(string(task), "\n") == "galos" {
-		fmt.Printf("blah %v\n", "1")
-	    /*
-	    if err := ctr("task", "remove", "--force", "galos"); err != nil {
-		    log.Print(err)
-	    }
-
-	    if err := ctr("snapshot", "remove", "galos"); err != nil {
-		    log.Print(err)
-	    }
-
-	    if err := ctr("container", "remove", "galos"); err != nil {
-		    log.Print(err)
-	    }
-	    */
-	}
-/*
-	if err := ctr("image", "pull", container); err != nil {
-		log.Print(err)
-	}
-
-	if err := ctr("container", "create",
-		"--privileged", "--net-host",
-		"--hostname", "galos",
-		"--mount", "type=bind,src=/perm/galos,dst=/perm,options=rbind:rw",
-		container, "galos"); err != nil {
-		return err
-	}
-
-	if err := ctr("task", "start", "--detach", "galos"); err != nil {
-		log.Print(err)
-	}
-*/
-	return nil
-}
-
 func main() {
-	makeDirectoryIfNotExists("/perm/galos")
-	if err := galos(); err != nil {
+	if err := redisExample(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-// expandPath returns env, but with PATH= modified or added
-// such that both /user and /usr/local/bin are included, which ctr needs.
-func expandPath(env []string) []string {
-	extra := "/user:/usr/local/bin"
-	found := false
-	for idx, val := range env {
-		parts := strings.Split(val, "=")
-		if len(parts) < 2 {
-			continue // malformed entry
-		}
-		key := parts[0]
-		if key != "PATH" {
-			continue
-		}
-		val := strings.Join(parts[1:], "=")
-		env[idx] = fmt.Sprintf("%s=%s:%s", key, extra, val)
-		found = true
+func redisExample() error {
+	// create a new client connected to the default socket path for containerd
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		return err
 	}
-	if !found {
-		const busyboxDefaultPATH = "/usr/local/sbin:/sbin:/usr/sbin:/usr/local/bin:/bin:/usr/bin"
-		env = append(env, fmt.Sprintf("PATH=%s:%s", extra, busyboxDefaultPATH))
-	}
-	return env
-}
+	defer client.Close()
 
-// https://gist.github.com/ivanzoid/5040166bb3f0c82575b52c2ca5f5a60c
-func makeDirectoryIfNotExists(path string) error {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return os.Mkdir(path, os.ModeDir|0755)
+	// create a new context with an "example" namespace
+	ctx := namespaces.WithNamespace(context.Background(), "example")
+
+	// pull the redis image from DockerHub
+	image, err := client.Pull(ctx, "docker.io/library/redis:alpine", containerd.WithPullUnpack)
+	if err != nil {
+		return err
 	}
+
+	// create a container
+	container, err := client.NewContainer(
+		ctx,
+		"redis-server",
+		containerd.WithImage(image),
+		containerd.WithNewSnapshot("redis-server-snapshot", image),
+		containerd.WithNewSpec(oci.WithImageConfig(image)),
+	)
+	if err != nil {
+		return err
+	}
+	defer container.Delete(ctx, containerd.WithSnapshotCleanup)
+
+	// create a task from the container
+	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
+	if err != nil {
+		return err
+	}
+	defer task.Delete(ctx)
+
+	// make sure we wait before calling start
+	exitStatusC, err := task.Wait(ctx)
+	if err != nil {
+		return err
+	}
+
+	// call start on the task to execute the redis server
+	if err := task.Start(ctx); err != nil {
+		return err
+	}
+
+	// sleep for a lil bit to see the logs
+	time.Sleep(3 * time.Second)
+
+	// kill the process and get the exit status
+	if err := task.Kill(ctx, syscall.SIGTERM); err != nil {
+		return err
+	}
+
+	// wait for the process to fully exit and print out the exit status
+
+	status := <-exitStatusC
+	code, _, err := status.Result()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("redis-server exited with status: %d\n", code)
+
 	return nil
 }
