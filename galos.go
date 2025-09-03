161a0a6152918e9efe6b977d8411d41e6e8f0d9e
ben-auo <180770701+ben-auo@users.noreply.github.com>
2025-09-02 22:21:45 -0700

    refactor

--- galos.go
+++ galos.go
@@ -9,7 +9,9 @@ import (
 	"github.com/gokrazy/gokrazy"
 )
 
-func run(exe string, args ...string) {
+var container = "docker.io/library/hello-world:latest"
+
+func run(logging bool, exe string, args ...string) {
 	cmd := execute.ExecTask{
 		Command:     exe,
 		Args:        args,
@@ -18,22 +20,43 @@ func run(exe string, args ...string) {
 
 	res, err := cmd.Execute(context.Background())
 	if err != nil {
-		panic(err)
+		if logging {
+			fmt.Errorf("Error: %v", err)
+		}
 	}
 
 	if res.ExitCode != 0 {
-		panic("Non-zero exit code: " + res.Stderr)
+		if logging {
+			fmt.Errorf("Error: %v", res.Stderr)
+		}
 	}
 
-	fmt.Printf("stdout: %s, stderr: %s, exit-code: %d\n", res.Stdout, res.Stderr, res.ExitCode)
+	if logging {
+		fmt.Printf("stdout: %s, stderr: %s, exit-code: %d\n", res.Stdout, res.Stderr, res.ExitCode)
+	}
 }
 
 func main() {
+	// create mount point
+	run(false, "/usr/local/bin/busybox", "mkdir", "-p", "/perm/galos")
+
 	// wait for network
 	gokrazy.WaitForClock()
 
 	// wait for containerd
 	time.Sleep(3 * time.Second)
 
-	run("/usr/local/bin/ctr", "version")
+	// remove prior container, if applicable
+	run(false, "/usr/local/bin/ctr", "task", "remove", "--force", "galos")
+	run(false, "/usr/local/bin/ctr", "snapshot", "remove", "galos")
+	run(false, "/usr/local/bin/ctr", "container", "remove", "galos")
+
+	// pull container
+	run(true, "/usr/local/bin/ctr", "image", "pull", container)
+
+	// create container
+	run(true, "/usr/local/bin/ctr", "container", "create", "--privileged", "--net-host", "--mount", "type=bind,src=/perm/galos,dst=/perm,options=rbind:rw", container, "galos")
+
+	// run container
+	run(true, "/usr/local/bin/ctr", "task", "start", "--detach", "galos")
 }
