0b606fa6205249a4e1762901152c4f4d0f151947
ben-auo <180770701+ben-auo@users.noreply.github.com>
2025-09-02 20:51:49 -0700

    go-execute

--- galos.go
+++ galos.go
@@ -1,128 +1,27 @@
 package main
 
-// forked from https://gokrazy.org/packages/docker-containers/
-
 import (
 	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"strings"
-	"context"
-	"time"
 
-	"github.com/gokrazy/gokrazy"
-	"github.com/go-cmd/cmd"
+	execute "github.com/alexellis/go-execute/v2"
+	"context"
 )
 
-var container = "docker.io/library/hello-world:latest"
-
-func ctr(args ...string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-	defer cancel()
-	ctr := exec.CommandContext(ctx, "/usr/local/bin/ctr", args...)
-	ctr.Env = expandPath(os.Environ())
-	ctr.Stdin = os.Stdin
-	ctr.Stdout = os.Stdout
-	ctr.Stderr = os.Stderr
-	if err := ctr.Run(); err != nil {
-		return fmt.Errorf("%v: %v", ctr.Args, err)
-	}
-	return nil
-}
-
-func galos() error {
-	// Ensure we have an up-to-date clock, which in turn also means that
-	// networking is up.
-	gokrazy.WaitForClock()
-
-	// Run foo and block waiting for it to exit
-	c := cmd.NewCmd("ls /")
-	s := <-c.Start()
-	log.Print(s)
-
-	// wait a few seconds for containerd to initialize
-	time.Sleep(3 * time.Second)
-
-	task, err := exec.Command("/usr/local/bin/ctr", "task", "list", "--quiet").Output()
-	if err != nil {
-		log.Print(err)
-	}
-
-	fmt.Printf("blah task: %v\n", string(task))
-
-	if strings.TrimRight(string(task), "\n") == "galos" {
-		fmt.Printf("blah %v\n", "1")
-	    /*
-	    if err := ctr("task", "remove", "--force", "galos"); err != nil {
-		    log.Print(err)
-	    }
-
-	    if err := ctr("snapshot", "remove", "galos"); err != nil {
-		    log.Print(err)
-	    }
-
-	    if err := ctr("container", "remove", "galos"); err != nil {
-		    log.Print(err)
-	    }
-	    */
-	}
-/*
-	if err := ctr("image", "pull", container); err != nil {
-		log.Print(err)
-	}
-
-	if err := ctr("container", "create",
-		"--privileged", "--net-host",
-		"--hostname", "galos",
-		"--mount", "type=bind,src=/perm/galos,dst=/perm,options=rbind:rw",
-		container, "galos"); err != nil {
-		return err
-	}
-
-	if err := ctr("task", "start", "--detach", "galos"); err != nil {
-		log.Print(err)
-	}
-*/
-	return nil
-}
-
 func main() {
-	makeDirectoryIfNotExists("/perm/galos")
-	if err := galos(); err != nil {
-		log.Fatal(err)
+	cmd := execute.ExecTask{
+		Command:     "ctr",
+		Args:        []string{"version"},
+		StreamStdio: false,
 	}
-}
 
-// expandPath returns env, but with PATH= modified or added
-// such that both /user and /usr/local/bin are included, which ctr needs.
-func expandPath(env []string) []string {
-	extra := "/user:/usr/local/bin"
-	found := false
-	for idx, val := range env {
-		parts := strings.Split(val, "=")
-		if len(parts) < 2 {
-			continue // malformed entry
-		}
-		key := parts[0]
-		if key != "PATH" {
-			continue
-		}
-		val := strings.Join(parts[1:], "=")
-		env[idx] = fmt.Sprintf("%s=%s:%s", key, extra, val)
-		found = true
+	res, err := cmd.Execute(context.Background())
+	if err != nil {
+		panic(err)
 	}
-	if !found {
-		const busyboxDefaultPATH = "/usr/local/sbin:/sbin:/usr/sbin:/usr/local/bin:/bin:/usr/bin"
-		env = append(env, fmt.Sprintf("PATH=%s:%s", extra, busyboxDefaultPATH))
-	}
-	return env
-}
 
-// https://gist.github.com/ivanzoid/5040166bb3f0c82575b52c2ca5f5a60c
-func makeDirectoryIfNotExists(path string) error {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return os.Mkdir(path, os.ModeDir|0755)
+	if res.ExitCode != 0 {
+		panic("Non-zero exit code: " + res.Stderr)
 	}
-	return nil
+
+	fmt.Printf("stdout: %s, stderr: %s, exit-code: %d\n", res.Stdout, res.Stderr, res.ExitCode)
 }
