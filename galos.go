72645791322f364586dfa057029094278abb049c
ben-auo <180770701+ben-auo@users.noreply.github.com>
2025-09-03 19:50:34 -0700

    refactor

--- galos.go
+++ galos.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"context"
+	"log"
 
 	execute "github.com/alexellis/go-execute/v2"
 	"github.com/gokrazy/gokrazy"
 )
 
 var container = "docker.io/library/hello-world:latest"
+//var executable = ""
+//var arguments = ""
 
 func run(logging bool, exe string, args ...string) {
 	cmd := execute.ExecTask{
@@ -20,31 +22,26 @@ func run(logging bool, exe string, args ...string) {
 
 	res, err := cmd.Execute(context.Background())
 	if err != nil {
-		if logging {
-			fmt.Errorf("Error: %v", err)
-		}
+		fmt.Errorf("Error: %v", err)
 	}
 
 	if res.ExitCode != 0 {
-		if logging {
-			fmt.Errorf("Error: %v", res.Stderr)
-		}
+		fmt.Errorf("Error: %v", res.Stderr)
 	}
 
 	if logging {
-		fmt.Printf("stdout: %s, stderr: %s, exit-code: %d\n", res.Stdout, res.Stderr, res.ExitCode)
+		fmt.Printf(res.Stdout)
 	}
 }
 
 func main() {
+	log.Print("Initializing...")
+
 	// create mount point
 	run(false, "/usr/local/bin/busybox", "mkdir", "-p", "/perm/galos")
 
 	// wait for network
-	gokrazy.WaitForClock()
-
-	// wait for containerd
-	time.Sleep(3 * time.Second)
+	gokrazy.WaitFor("net-online")
 
 	// remove prior container, if applicable
 	run(false, "/usr/local/bin/ctr", "task", "remove", "--force", "galos")
@@ -52,11 +49,18 @@ func main() {
 	run(false, "/usr/local/bin/ctr", "container", "remove", "galos")
 
 	// pull container
-	run(true, "/usr/local/bin/ctr", "image", "pull", container)
+	fmt.Printf("Pulling container image. Please wait, this may take a while...")
+	run(false, "/usr/local/bin/ctr", "image", "pull", container)
 
 	// create container
-	run(true, "/usr/local/bin/ctr", "container", "create", "--privileged", "--net-host", "--mount", "type=bind,src=/perm/galos,dst=/perm,options=rbind:rw", container, "galos")
+	fmt.Printf("Creating container...")
+	run(false, "/usr/local/bin/ctr", "container", "create", "--privileged", "--net-host", "--mount", "type=bind,src=/perm/galos,dst=/perm,options=rbind:rw", container, "galos")
 
 	// run container
-	run(true, "/usr/local/bin/ctr", "task", "start", "--detach", "galos")
+	fmt.Printf("Running container...")
+	run(true, "/usr/local/bin/ctr", "task", "start", "galos")
+
+	// show results
+	fmt.Printf("Done:")
+	run(true, "/usr/local/bin/ctr", "task", "exec", "--exec-id", "oneshot", "galos", "ps aux | head -n 2")
 }
